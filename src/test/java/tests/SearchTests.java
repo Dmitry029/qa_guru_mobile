bd706f3bc2584ee3dfd32dd08061414b3e312cac
Dmitry029 <kdp.029029@gmail.com>
2026-09-07 14:09:05 +0300

    Убрал строку2

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -57,7 +57,6 @@ public class SearchTests {
             "android.widget.TextView"));
         assert (!allProductsName.isEmpty());
 
-
         // Invoke driver.quit() after the test is done to indicate that the test is completed.
         driver.quit();
     }
